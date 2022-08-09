85074b883d021cde53ff729ea683892d12c57bc5
BoBRoss <zenttosik1997@mail.ru>
2022-08-09 21:12:40 +0500

    Готовое дз4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
+    //Задание 1
 
-
+        int n = 1;
+    while (n <=10) {
+        System.out.print(n++);
     }
-}
+    System.out.println();
+
+    for (int n1 =10; n1 >0; n1--) {
+        System.out.print(n1);
+    }
+System.out.println();
+
+    //Задание 2
+        int friday = 5;
+    for (;friday <= 31; friday += 7){
+            System.out.println("Сегодня пятница, " +friday + " -е число. Необходимо подготовить отчет");
+    }
+
+    //Задание 3
+    for (int y = 1822; y <= 2122; y++) {
+        if (y % 79 == 0) {
+            System.out.println(+y);
+        }
+        }
+        }
+    }
+
